5e382f5d930c14e3a06c0de5ad5cc41a40fc945e
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2016-01-06 19:21:54 +0100

    pics with usable license added.

--- kittens.js
+++ kittens.js
@@ -1,8 +1,20 @@
 var kittens = [
   {
-    "url": "https://www.flickr.com/photos/ulfbjolin/8546428010/",
-    "title": "Sphynx cat Mållgan by Ulf Bjolin, on Flickr",
-    "imageUrl": "https://farm9.staticflickr.com/8239/8546428010_9c5af49348_o.jpg",
-    "imageAlt": "Our sphynx resting in the sunlight."
+    "url": "https://www.flickr.com/photos/pivic/3256959923/",
+    "title": "Sphynx cuties",
+    "imageUrl": "https://farm4.staticflickr.com/3373/3256959923_044f6cedb0_b.jpg",
+    "imageAlt": "My heart melts and is but a puddle on the floor."
+  },
+  {
+    "url": "https://www.flickr.com/photos/polycola/21804360169/",
+    "title": "Sphynx",
+    "imageUrl": "https://farm6.staticflickr.com/5681/21804360169_045d343a00_b.jpg",
+    "imageAlt": "Curious Sphynx cat."
+  },
+  {
+    "url": "https://www.flickr.com/photos/fleur-design/2555773089/",
+    "title": "Kinsey the Sphynx",
+    "imageUrl": "https://farm4.staticflickr.com/3262/2555773089_c63edcb5ac_o.jpg",
+    "imageAlt": "Our 8 month old cat Kinsey. He is a blue Sphynx. Seen here on a home built cat tree made from an old tree branch, rope and plywood."
   }
 ];
